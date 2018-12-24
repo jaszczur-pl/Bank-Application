2b4a81b67f2c9c84bed938b58b5eb6d29edf1011
mjaszczura <maciej.jaszczura17@gmail.com>
2018-12-24 17:31:11 +0100

    Added Customer Model with all necessary fields

---
+++ BankApplication/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsBlocked { get; set; }
+        public int IncorrectLogins { get; set; }
+    }
+}
